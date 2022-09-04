fc0959a3e80e86dd7d7106cfa72976cdb5aa6b25
Kevin M Grove <71804321+kgrove002@users.noreply.github.com>
2022-09-04 11:16:45 -0400

    Add files via upload

--- Data Analyst - JSON/main.py
+++ Data Analyst - JSON/main.py
@@ -5,8 +5,12 @@ with open("data.json", "r") as read_file:
     data = json.load(read_file)["data"]
 
 gradeBook = pandas.DataFrame(data)
-gradeBook1 = pandas.DataFrame(data)
+# gradeBook1 = pandas.DataFrame(data)
 
-with pandas.ExcelWriter("Grade Book.xlsx") as writer:
-    gradeBook.to_excel(writer, sheet_name='Grade_1')
-    gradeBook1.to_excel(writer, sheet_name='Grade_2')
+# Creates a new Excel File
+gradeBook.to_excel('Grade Book.xlsx')
+
+# Write to an existing sheet
+# with pandas.ExcelWriter("Grade Book 1.xlsx", engine="xlsxwriter") as writer:
+#     gradeBook.to_excel(writer, sheet_name='Grade_1')
+#     gradeBook1.to_excel(writer, sheet_name='Grade_2')
